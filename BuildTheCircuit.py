b21cf36adaa84937a27a013621e8d260cae74de0
Vašek Lacheta <47534064+Vistoun@users.noreply.github.com>
2021-01-04 09:19:17 +0100

    menu

--- BuildTheCircuit.py
+++ BuildTheCircuit.py
@@ -1,7 +1,7 @@
-import pygame
+import pygame, json
 from pygame.constants import MOUSEBUTTONDOWN, MOUSEBUTTONUP
 from pygame.event import event_name
-from Components import Resistor, Diode, Button
+from Components import Resistor, Diode, Button, PowerSupply, Switch
 
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
@@ -9,24 +9,44 @@ SCREEN_HEIGHT = 800
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+with open("data_build_circuit.json", "r") as components_file:
+    data = json.load(components_file)
 
-d1 = Diode(500, 150, 50)
-r1 = Resistor(600, 125, 160, 70, (255, 255, 255), "")
-r2 = Resistor(500, 610, 160, 70, (0, 0, 0), "R2")
-r3 = Resistor(225, 455, 70, 160, (0, 0, 0), "R3")
-r4 = Resistor(1100, 50, 70, 160, (0, 0, 0), "")
-
-components = (d1, r1, r2, r3, r4)
-components_to_choose = (r4)
+components = []
+for key in data.keys():
+    if key != "level_answer":
+        if data[key]["type"] == "Diode":
+            components.append(Diode(data[key]["name"], data[key]["value"], data[key]["left"],
+                                    data[key]["top"], data[key]["radius"]))
+        if data[key]["type"] == "Resistor":
+            components.append(Resistor(data[key]["name"], data[key]["value"], data[key]["left"],
+                                    data[key]["top"], data[key]["is_vertical"], data[key]["is_invisible"]))
+        if data[key]["type"] == "Switch":
+            components.append(Switch(data[key]["name"], data[key]["left"], data[key]["top"], data[key]["radius"],
+                                    data[key]["mode_on"]))
+        if data[key]["type"] == "PowerSupply":
+            components.append(PowerSupply(data[key]["name"], data[key]["value"], data[key]["left"], data[key]["top"]))
+    else:
+        level_answer = data[key]
 
 offset_x = 0
 offset_y = 0
 draging = False
 won_the_round = False
 running = True
+
+for choose_component in components:
+    if choose_component.name == "R1": 
+        r1 = choose_component
+        r1.name = ""
+    if choose_component.name == "R3":
+        r3 = choose_component
+        r3.name = "R?"
+        
+
 while running:
     mouse = pygame.mouse.get_pos()
-
+        
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running = False
@@ -35,29 +55,31 @@ while running:
         elif e.type == MOUSEBUTTONDOWN:
             if e.button == 1:
                 draging = True
-                if r4.collidepoint(e.pos):
+                if r3.collidepoint(e.pos):
                     mouse_x, mouse_y = e.pos
-                    offset_x = r4.x - mouse_x
-                    offset_y = r4.y - mouse_y
+                    offset_x = r3.x - mouse_x
+                    offset_y = r3.y - mouse_y
 
         elif e.type == MOUSEBUTTONUP:
             if e.button == 1:
                 draging = False
-                if r4.colliderect(r1):
-                    r4.width = 0
-                    r4.height = 0
-                    r1.color = (0, 0, 0)
+                if r3.colliderect(r1):
+                    r3.width = 0
+                    r3.height = 0
+                    r3.name = ""
+                    r1.color = (90,90,90)
+                    r1.name = "R1"
                     won_the_round = True
                     
                 else:
-                    r4.x = 1100
-                    r4.y = 50
+                    r3.x = 1100
+                    r3.y = 50
 
         elif e.type == pygame.MOUSEMOTION:
-            if draging and r4.collidepoint(e.pos):
+            if draging and r3.collidepoint(e.pos):
                 mouse_x, mouse_y = e.pos
-                r4.x = mouse_x + offset_x
-                r4.y = mouse_y + offset_y
+                r3.x = mouse_x + offset_x
+                r3.y = mouse_y + offset_y
 
     # Background color
     screen.fill((255, 255, 255))
@@ -76,7 +98,7 @@ while running:
         screen.blit(win_screen, ((SCREEN_WIDTH - win_screen.get_width())/2 , (SCREEN_HEIGHT - win_screen.get_height())/2))
 
     for component in components:
-        component.Draw(screen)
+        component.draw(screen)
 
     pygame.display.flip()
     pygame.display.update()
--- Components.py
+++ Components.py
@@ -1,4 +1,8 @@
 import pygame
+from math import sqrt
+
+COLOR_BLACK = (0, 0, 0)
+COLOR_WHITE = (255, 255, 255)
 
 
 class Button(pygame.Rect):
@@ -20,7 +24,7 @@ class Button(pygame.Rect):
     def is_clickable(self, mouse):
         return self.left <= mouse[0] <= self.left + self.width and self.top <= mouse[1] <= self.top + self.height
 
-    def Draw(self, mouse, screen):
+    def draw(self, mouse, screen):
         self.state_color = self.light_color if self.is_clickable(
             mouse) else self.dark_color
         pygame.draw.rect(screen, self.state_color, [
@@ -29,25 +33,30 @@ class Button(pygame.Rect):
                     (self.left + 20, self.top + 30))
 
 
-class Diode():
-    def __init__(self, x, y, radius):
-        super(Diode,self).__init__()
-        self.x = x
-        self.y = y
+class Diode:
+    def __init__(self, name, value, left, top, radius):
+        super(Diode, self).__init__()
+        self.name = name
+        self.__value = value
+        self.unit = "W"
+        self.font = pygame.font.SysFont('Consolas', 40)
+        self.left = left
+        self.top = top
         self.radius = radius
         self.__switch_on = (255, 255, 0)
-        self.__switch_off = (255, 255, 255)
+        self.__switch_off = COLOR_WHITE
         self.light = self.switch_off
 
-    def Draw(self, screen):
-        pygame.draw.circle(screen, (0, 0, 0), (self.x, self.y), self.radius)
+    def draw(self, screen):
+        pygame.draw.circle(screen, COLOR_BLACK, (self.left, self.top), self.radius)
         pygame.draw.circle(screen, self.light,
-                           (self.x, self.y), self.radius - 5)
+                           (self.left, self.top), self.radius - 5)
 
-        pygame.draw.line(screen, (0, 0, 0), (self.x - self.radius / 1.5, self.y - self.radius / 1.5),
-                         (self.x + self.radius / 1.5, self.y + self.radius / 1.5), 7)
-        pygame.draw.line(screen, (0, 0, 0), (self.x - self.radius / 1.5, self.y + self.radius / 1.5),
-                         (self.x + self.radius / 1.5, self.y - self.radius / 1.5), 7)
+        pygame.draw.line(screen, COLOR_BLACK, (self.left - self.radius / 1.5, self.top - self.radius / 1.5),
+                         (self.left + self.radius / 1.5, self.top + self.radius / 1.5), 7)
+        pygame.draw.line(screen, COLOR_BLACK, (self.left - self.radius / 1.5, self.top + self.radius / 1.5),
+                         (self.left + self.radius / 1.5, self.top - self.radius / 1.5), 7)
+        screen.blit(self.font.render(self.name, True, COLOR_BLACK), (self.left, self.top+self.radius))
 
     @property
     def switch_on(self):
@@ -63,21 +72,132 @@ class Diode():
     def off(self):
         self.light = self.switch_off
 
+    @property
+    def value(self):
+        return self.__value
+
 
 class Resistor(pygame.Rect):
-    def __init__(self, left, top, width, height, color, name):
-        super(Resistor,self).__init__(self)
+    def __init__(self, name, value, left, top, is_vertical=False, is_invisible=False, width=160, height=70 ):
+        super(Resistor, self).__init__(self)
+        self.name = name
+        self.font = pygame.font.SysFont('Consolas', 40)
+        self.__value = value
+        self.unit = '\u03A9'
         self.left = left
         self.top = top
         self.width = width
         self.height = height
+        if is_vertical:
+            temp = self.width
+            self.width = self.height
+            self.height = temp
 
-        self.color = color
+        if is_invisible:
+            self.color = (255, 255, 255)
+        else:
+            self.color = (90, 90, 90)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, [self.left, self.top, self.width, self.height])
+        screen.blit(self.font.render(self.name, True, COLOR_BLACK), (self.left, self.top))
+
+    @property
+    def value(self):
+        return self.__value
+
+
+class Switch(pygame.Rect):
+    def __init__(self, name, left, top, radius, mode_on=1):
+        super(Switch, self).__init__(self)
         self.name = name
         self.font = pygame.font.SysFont('Consolas', 40)
+        self.left = left
+        self.top = top
+        self.radius = radius
+        self.mode = mode_on
 
-    def Draw(self, screen):
+    def draw(self, screen):
+        pygame.draw.circle(screen, COLOR_BLACK, (self.left, self.top), self.radius)
+        pygame.draw.circle(screen, COLOR_WHITE, (self.left, self.top), self.radius-3)
+
+        pygame.draw.circle(screen, COLOR_BLACK, (self.left+65, self.top), self.radius)
+        pygame.draw.circle(screen, COLOR_WHITE, (self.left+65, self.top), self.radius-3)
+
+        pygame.draw.line(screen, COLOR_WHITE, (self.left+self.radius-1, self.top),
+                         (self.left+65-self.radius, self.top), 15)
+        pygame.draw.line(screen, COLOR_BLACK, (self.left, self.top-self.radius),
+                         (self.left + 65, self.top-(self.radius*self.mode)), 5)          
+        screen.blit(self.font.render(self.name, True, COLOR_BLACK), (self.left, self.top+self.radius))
+
+
+class PowerSupply:
+    def __init__(self, name, value, left, top):
+        self.name = name
+        self.font = pygame.font.SysFont('Consolas', 40)
+        self.value = value
+        self.unit = "V"
+        self.left = left
+        self.top = top
+        self.small_offset = 20
+        self.big_offset = 50
+
+    def draw(self, screen):
+        pygame.draw.line(screen, COLOR_BLACK, (self.left, self.top - self.big_offset),
+                         (self.left, self.top + self.big_offset), 5)
+        pygame.draw.line(screen, COLOR_WHITE, (self.left+3, self.top),
+                         (self.left+35, self.top), 15)
+        pygame.draw.line(screen, COLOR_BLACK, (self.left+35, self.top-self.small_offset),
+                         (self.left+35, self.top+self.small_offset), 5)
+        screen.blit(self.font.render(self.name, True, COLOR_BLACK), (self.left, self.top+self.big_offset))
+
+
+class MultiMeter(pygame.Rect):
+    def __init__(self, left, top, width, height):
+        super(MultiMeter, self).__init__(self)
+        # multimeter's properties
+        self.left = left
+        self.top = top
+        self.width = width
+        self.height = height
+        self.color = (255, 90, 90)
+        self.font = pygame.font.SysFont('Consolas', 28)
+        self.display_font = pygame.font.SysFont('Consolas', 50)
+        self.name = "Multi-o-meter"
+        self.display_text = "0.0"
+
+        # pin's properties
+        self.pin_left = self.left+75
+        self.pin_top = self.top+self.height+50
+        self.pin_radius = 15
+
+    def draw(self, screen):
         pygame.draw.rect(screen, self.color, [self.left, self.top, self.width, self.height])
-        screen.blit(self.font.render(self.name, True,
-                                     (0, 0, 0)), (self.left, self.top))
+        pygame.draw.rect(screen, COLOR_WHITE, [self.left+10, self.top+10, 180, 100])
 
+        screen.blit(self.font.render(self.name, True, COLOR_BLACK), (self.left, self.top+self.height/2))
+        screen.blit(self.display_font.render(self.display_text, True, COLOR_BLACK), (self.left+15, self.top+50))
+        pygame.draw.line(screen, COLOR_BLACK, (self.left+75, self.top + self.height-50),
+                         (self.pin_left, self.pin_top), 5)
+        pygame.draw.circle(screen, COLOR_BLACK, (self.pin_left, self.pin_top), self.pin_radius)
+
+    def collide_with_mouse(self, mouse):
+        hypotenuse = sqrt(((self.pin_left-mouse[0])**2) + ((self.pin_top-mouse[1])**2))
+        return hypotenuse <= self.pin_radius
+
+    def is_colliding(self, other_object):
+        if type(other_object) is Diode:
+            hypotenuse = sqrt(((self.pin_left - other_object.left)**2) + ((self.pin_top - other_object.top)**2))
+            return hypotenuse <= self.pin_radius + other_object.radius
+
+        if type(other_object) is Resistor:
+            return other_object.left <= self.pin_left <= other_object.left + other_object.width and \
+                    other_object.top <= self.pin_top <= other_object.top + other_object.height
+
+        if type(other_object) is PowerSupply:
+            return other_object.left <= self.pin_left <= other_object.left+35 and \
+                   other_object.top-50 <= self.pin_top <= other_object.top+50
+
+    def follow_mouse(self, mouse):
+        self.pin_left = mouse[0]
+        self.pin_top = mouse[1]

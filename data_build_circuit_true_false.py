1fb563e3700f20c37e218fbfa40334ee8edcf754
Viktor Zwinger <45140691+GedTest@users.noreply.github.com>
2021-01-09 08:46:42 +0100

    Added data file

---
+++ data_build_circuit_true_false.py
@@ -0,0 +1,169 @@
+task1 = [
+    {
+        "type": "Diode",
+        "name": "D1",
+        "value": 60.0,
+        "left": 500,
+        "top": 150,
+        "radius": 50
+    },
+    {
+        "type": "Resistor",
+        "name": "R1",
+        "left": 600,
+        "top": 125,
+        "value": 60.0,
+        "is_vertical": False,
+        "is_invisible": False
+    },
+    {
+        "type": "Resistor",
+        "name": "R2",
+        "left": 500,
+        "top": 610,
+        "value": 10.0,
+        "is_vertical": False,
+        "is_invisible": False
+    },
+    {
+        "type": "Resistor",
+        "name": "R3",
+        "left": 225,
+        "top": 455,
+        "value": 20.0,
+        "is_vertical": True,
+        "is_invisible": False
+    },
+    {
+        "type": "Switch",
+        "name": "S1",
+        "left": 325,
+        "top": 645,
+        "radius": 15,
+        "mode_on": 1
+    },
+    {
+        "type": "Switch",
+        "name": "S2",
+        "left": 750,
+        "top": 645,
+        "radius": 15,
+        "mode_on": 2
+    },
+    {
+        "type": "PowerSupply",
+        "name": "PS",
+        "value": 230.0,
+        "left": 300,
+        "top": 155
+    },
+    {
+        "level_answer": "No"
+    }
+]
+
+task2 = [
+    {
+        "type": "Diode",
+        "name": "D1",
+        "value": 60.0,
+        "left": 500,
+        "top": 150,
+        "radius": 50
+    },
+    {
+        "type": "Diode",
+        "name": "D2",
+        "value": 60.0,
+        "left": 255,
+        "top": 545,
+        "radius": 50
+    },
+    {
+        "type": "Diode",
+        "name": "D3",
+        "value": 60.0,
+        "left": 500,
+        "top": 645,
+        "radius": 50
+    },
+    {
+        "type": "Resistor",
+        "name": "R1",
+        "left": 600,
+        "top": 125,
+        "value": 60.0,
+        "is_vertical": False,
+        "is_invisible": False
+    },
+    {
+        "type": "Resistor",
+        "name": "R2",
+        "left": 755,
+        "top": 610,
+        "value": 10.0,
+        "is_vertical": False,
+        "is_invisible": False
+    },
+    {
+        "type": "Resistor",
+        "name": "R3",
+        "left": 225,
+        "top": 285,
+        "value": 20.0,
+        "is_vertical": True,
+        "is_invisible": False
+    },
+    {
+        "type": "Switch",
+        "name": "S1",
+        "left": 815,
+        "top": 155,
+        "radius": 15,
+        "mode_on": 1
+    },
+    {
+        "type": "PowerSupply",
+        "name": "PS",
+        "value": 230.0,
+        "left": 300,
+        "top": 155
+    },
+    #"Yes"
+]
+
+task3 = [
+    {
+        "type": "Diode",
+        "name": "D1",
+        "value": 60.0,
+        "left": 500,
+        "top": 150,
+        "radius": 50
+    },
+    {
+        "type": "Resistor",
+        "name": "R1",
+        "left": 600,
+        "top": 125,
+        "value": 60.0,
+        "is_vertical": False,
+        "is_invisible": False
+    },
+    {
+        "type": "Switch",
+        "name": "S1",
+        "left": 815,
+        "top": 155,
+        "radius": 15,
+        "mode_on": 1
+    },
+    {
+        "type": "PowerSupply",
+        "name": "PS",
+        "value": 230.0,
+        "left": 300,
+        "top": 155
+    },
+    #"Yes"
+]
